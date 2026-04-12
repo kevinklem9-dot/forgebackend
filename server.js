abad666263bca04230e375bc4d41d1ad1314e517
kevinklem9-dot <kevinklem9@gmail.com>
2026-04-12 16:48:13 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const corsOptions = {
     callback(new Error('Not allowed by CORS'));
   },
   credentials: true,
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization']
 };
 app.use(helmet({ contentSecurityPolicy: false })); // Security headers
